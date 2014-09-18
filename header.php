30f60023b2f548d22e27275af19e077943a2d18f
Dave Allen <davidallen401@btinternet.com>
2014-09-18 16:21:47 +0100

    Header image

--- header.php
+++ header.php
@@ -28,17 +28,16 @@
 
 	<header id="masthead" class="site-header" role="banner">
 
-<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/logo-stweed-dev-01.png" alt="home page" /> </a>
+<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/logo-stweed-dev-white.png" alt="home page" /> </a>
 
 		<div class="site-branding">
 			<h1 class="site-title visuallyhidden"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description visuallyhidden"><?php bloginfo( 'description' ); ?></h2>
 		</div>
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+	
+	</header><!-- #masthead -->
+<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Menu', 'tweedthree' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
 	<div id="content" class="site-content">
